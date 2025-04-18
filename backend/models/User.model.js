1c646ac76624e86ae217004b6b39ef8bfcf340a7
Pragati261999 <pragati1999tiwari@gmail.com>
2025-04-18 23:08:05 +0530

    backend quiz models

--- backend/models/User.model.js
+++ backend/models/User.model.js
@@ -1,8 +1,8 @@
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   name: String,
-  email: String,
+  email: { type: String, unique: true },
+  password: String,
+  role: { type: String, enum: ["admin", "student"], default: "student" },
 });
-
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
